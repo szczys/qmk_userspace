3302d26b90f30ee5ec407bdb9c885f21ed76584a
Mike Szczys <szczys@hotmail.com>
2024-08-07 23:16:09 -0400

    oled: add layer names and corne logo
    Signed-off-by: Mike Szczys <szczys@hotmail.com>

--- keyboards/crkbd/rev1/keymaps/szczys_corne_mx/keymap.c
+++ keyboards/crkbd/rev1/keymaps/szczys_corne_mx/keymap.c
@@ -178,3 +178,49 @@ bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
             return false;
     }
 }
+
+#ifdef OLED_ENABLE
+
+void oled_render_logo(void) {
+    // clang-format off
+    static const char PROGMEM crkbd_logo[] = {
+        0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8a, 0x8b, 0x8c, 0x8d, 0x8e, 0x8f, 0x90, 0x91, 0x92, 0x93, 0x94,
+        0xa0, 0xa1, 0xa2, 0xa3, 0xa4, 0xa5, 0xa6, 0xa7, 0xa8, 0xa9, 0xaa, 0xab, 0xac, 0xad, 0xae, 0xaf, 0xb0, 0xb1, 0xb2, 0xb3, 0xb4,
+        0xc0, 0xc1, 0xc2, 0xc3, 0xc4, 0xc5, 0xc6, 0xc7, 0xc8, 0xc9, 0xca, 0xcb, 0xcc, 0xcd, 0xce, 0xcf, 0xd0, 0xd1, 0xd2, 0xd3, 0xd4,
+        0};
+    // clang-format on
+    oled_write_P(crkbd_logo, false);
+}
+
+bool oled_task_user(void) {
+    if (is_keyboard_master()) {
+        oled_write_P(PSTR("Layer: "), false);
+
+        switch (get_highest_layer(layer_state)) {
+            case _QWERTY:
+                oled_write_P(PSTR("Default\n"), false);
+                break;
+            case _LOWER:
+                oled_write_P(PSTR("Lower\n"), false);
+                break;
+            case _RAISE:
+                oled_write_P(PSTR("Raise\n"), false);
+                break;
+            case _NUMPAD:
+                oled_write_P(PSTR("Numpad\n"), false);
+                break;
+            case _FUNC:
+                oled_write_P(PSTR("Function\n"), false);
+                break;
+            default:
+                // Or use the write_ln shortcut over adding '\n' to the end of your string
+                oled_write_ln_P(PSTR("Undefined"), false);
+        }
+    } else {
+        oled_render_logo();  // Renders a static logo
+    }
+
+    return false;
+}
+
+#endif
